946e937d98d11e62cdfcb0e69747e00dfadd38a8
Dan David <daviddan_09@yahoo.com>
2018-11-15 22:54:06 +0200

    Added Form

--- src/app/components/shared/auth/register/register.component.ts
+++ src/app/components/shared/auth/register/register.component.ts
@@ -1,15 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import {FormControl, FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  model: any = {};
+  loading = false;
+  returnUrl: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    // reset register status
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  register() {
+
+  }
 }
