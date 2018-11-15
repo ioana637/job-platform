df8e1218247ca8d0e2af43d1e69bfafbb5c7301b
Catalin Mates <catalin.mates@tss-yonder.com>
2018-11-15 09:22:45 +0200

    Add login form

--- src/app/components/shared/auth/login/login.component.ts
+++ src/app/components/shared/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  model: any = {};
+  loading = false;
+  returnUrl: string;
+
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router) { }
 
   ngOnInit() {
+    // reset login status
+
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/';
   }
 
+  login() {
+
+  }
 }
