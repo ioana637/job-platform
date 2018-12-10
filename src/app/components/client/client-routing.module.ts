a1ed0965508a27b5102377dc599d3c06880414f2
Catalin Mates <catalin.mates@yahoo.com>
2018-12-10 20:13:35 +0200

    Provider list frontend layout

--- src/app/components/client/client-routing.module.ts
+++ src/app/components/client/client-routing.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {AddJobComponent} from './add-job/add-job.component';
 import {ClientComponent} from './client/client.component';
+import { ProviderListComponent } from './provider-list/provider-list.component';
 
 const routes = [
   {
@@ -11,6 +12,10 @@ const routes = [
   {
     path: 'client',
     component: ClientComponent
+  },
+  {
+    path: 'client/providerList',
+    component: ProviderListComponent
   }
 ];
 
--- src/app/components/client/client.module.ts
+++ src/app/components/client/client.module.ts
@@ -11,6 +11,8 @@ import {ToastModule} from 'primeng/toast';
 import {ClientComponent} from './client/client.component';
 import {ClientRoutingModule} from './client-routing.module';
 import { ToolbarClientComponent } from './toolbar-client/toolbar-client.component';
+import { ProviderListComponent } from './provider-list/provider-list.component';
+import { ProviderCardComponent } from './provider-card/provider-card.component';
 
 @NgModule({
   imports: [
@@ -28,6 +30,8 @@ import { ToolbarClientComponent } from './toolbar-client/toolbar-client.componen
   ],
   declarations: [
     AddJobComponent,
+    ProviderListComponent,
+    ProviderCardComponent,
     ClientComponent,
     ToolbarClientComponent,
     ToolbarClientComponent
@@ -36,7 +40,9 @@ import { ToolbarClientComponent } from './toolbar-client/toolbar-client.componen
   providers: [MessageService],
   entryComponents: [
     ToolbarClientComponent,
-    AddJobComponent
+    AddJobComponent,
+    ProviderListComponent,
+    ProviderCardComponent,
   ]
 })
 export class ClientModule {
