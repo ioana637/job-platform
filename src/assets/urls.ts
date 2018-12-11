6906a9edd10589517b8dab00b5b084b72dc9b950
Dan David <daviddan_09@yahoo.com>
2018-12-11 10:18:35 +0200

    Added server request for register

---
+++ src/assets/urls.ts
@@ -0,0 +1,9 @@
+const baseUrl = 'http://localhost:8080';
+export const jobUrl = `${baseUrl}/job`;
+export const loginUrl = `${baseUrl}/login`;
+export const registerUrl = `${baseUrl}/register`;
+export const abilityUrl = `${baseUrl}/ability`;
+export const levelUrl = `${abilityUrl}/levels`;
+
+
+
