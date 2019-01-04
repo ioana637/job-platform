c18f34740637cd6aafa2a91e3cd002c1b4c4738d
Damian Roxana <damianroxi@gmail.com>
2019-01-04 16:00:18 +0200

    statistici partea stanga a ecranului

---
+++ src/app/components/shared/statistici-left/statistici-left.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StatisticiLeftService {
+
+  constructor(private http: HttpClient) { }
+
+  getAngajati(){
+    return this.http.get<any>("http://localhost:8080/statistics/clients")
+  }
+
+  getAngajatori(){
+    return this.http.get<any>("http://localhost:8080/statistics/providers")
+  }
+}
