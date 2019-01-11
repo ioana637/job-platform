cf706d3716976b0912a424a638f0894f8b548998
Geanina Cosovanu <cosovanugeanina@gmail.com>
2019-01-11 18:37:47 +0200

    fix bug logout->login

--- src/app/components/shared/auth/login/login.component.ts
+++ src/app/components/shared/auth/login/login.component.ts
@@ -8,7 +8,7 @@ import {MessageService} from 'primeng/api';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
   form: FormGroup;
